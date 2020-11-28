2264faf7d68d6e57d5b74f2dfb368f37b3777307
BaxanAcharya <baxanacharya@gmail.com>
2020-11-28 12:47:33 +0545

    day_4 additional configuration

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-
-app.listen(3010, () => {
-  console.log(`Server running in 3010`);
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+require("dotenv").config({
+  path: "./config/server.env",
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World!!!!");
+});
+
+//Page not found
+app.use((req, res) => {
+  res.status(404).json({
+    msg: "Page Not Found",
+  });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running in ${PORT}`);
 });
